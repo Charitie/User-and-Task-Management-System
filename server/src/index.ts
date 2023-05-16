3c083bfcf2eff69f2e901a2e499dc389c2279474
charity.tarus <charity.tarus@computercastlesltd.co.ke>
2023-05-16 12:51:32 +0300

    Setup prettier with eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,15 @@
 import http from 'http';
 
 export const server = http.createServer((req, res) => {
-  res.writeHead(200, {'content-type': 'application/json'});
-//   console.log('Hello');
+  res.writeHead(200, { 'content-type': 'application/json' });
+  //   console.log('Hello');
   for (let i = 0; i < 10; i++) {
     // console.log(i);
   }
   res.end(
     JSON.stringify({
-      data: 'It works!!',
-    }),
+      data: 'It works!!'
+    })
   );
 });
 
