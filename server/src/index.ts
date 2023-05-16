c058ed5731793890b6c54c9959ce6784a8a19470
charity.tarus <charity.tarus@computercastlesltd.co.ke>
2023-05-16 12:42:01 +0300

    Setup eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
 import http from 'http';
 
-export const server= http.createServer((req, res)=>{
-    res.writeHead(200, {"Contet-type": "application/json"});
-    res.end(
-        JSON.stringify({
-            data:'It works!!'
-        })
-    )
-})
+export const server = http.createServer((req, res) => {
+  res.writeHead(200, {'content-type': 'application/json'});
+//   console.log('Hello');
+  for (let i = 0; i < 10; i++) {
+    // console.log(i);
+  }
+  res.end(
+    JSON.stringify({
+      data: 'It works!!',
+    }),
+  );
+});
 
-server.listen(5000, ()=> console.log('Server running on port 5000'))
+server.listen(5000, () => console.log('Server running on port 5000'));
